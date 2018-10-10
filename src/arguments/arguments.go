63a9c5565b23eacce487cccc3e7cf3907e97b5db
Camden Cheek <ccheek22@gmail.com>
2018-10-10 11:07:18 -0400

    Add Parse and default array

--- src/arguments/arguments.go
+++ src/arguments/arguments.go
@@ -1,6 +1,9 @@
 package arguments
 
 import (
+  "regexp"
+  "errors"
+  "encoding/json"
 	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
 )
 
@@ -15,6 +18,44 @@ type ArgumentList struct {
 	UseSSL           bool   `default:"true" help:"Whether or not to use SSL to connect to the API. The F5 API only allows connections using SSL."`
 	CABundleFile     string `default:"" help:"Alternative Certificate Authority bundle file"`
 	CABundleDir      string `default:"" help:"Alternative Certificate Authority bundle directory"`
-	PoolMemberFilter string `default:"" help:"JSON array of pool member name regexes to collect."`
-	NodeFilter       string `default:"" help:"JSON array of node name regexes to collect."`
+	PoolMemberFilter string `default:"[]" help:"JSON array of pool member name regexes to collect."`
+	NodeFilter       string `default:"[]" help:"JSON array of node name regexes to collect."`
+}
+
+// Parse validates and parses out regex patterns from the input arguments
+func (al *ArgumentList) Parse() ([]*regexp.Regexp, []*regexp.Regexp, error) {
+  if al.Username == "" || al.Password == "" {
+    return nil, nil, errors.New("both username and password must be provided")
+  }
+
+  var poolMemberRegexStrings []string
+  if err := json.Unmarshal([]byte(al.PoolMemberFilter), &poolMemberRegexStrings); err != nil {
+    return nil, nil, err
+  }
+
+  poolMemberRegexPatterns := make([]*regexp.Regexp, len(poolMemberRegexStrings))
+  for i, regexString := range poolMemberRegexStrings {
+    pattern, err := regexp.Compile(regexString)
+    if err != nil {
+      return nil, nil, err
+    }
+    poolMemberRegexPatterns[i] = pattern
+  }
+
+  var nodeRegexStrings []string
+  if err := json.Unmarshal([]byte(al.NodeFilter), &nodeRegexStrings); err != nil {
+    return nil, nil, err
+  }
+
+  nodeRegexPatterns := make([]*regexp.Regexp, len(nodeRegexStrings))
+  for i, regexString := range nodeRegexStrings {
+    pattern, err := regexp.Compile(regexString)
+    if err != nil {
+      return nil, nil, err
+    }
+    nodeRegexPatterns[i] = pattern
+  }
+
+  return poolMemberRegexPatterns, nodeRegexPatterns, nil
+
 }
