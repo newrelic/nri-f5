5e2481936076969c9e0e9cb3de4c99468d28437b
paologallinaharbur <paologallina1992@gmail.com>
2021-01-15 15:36:39 +0100

    fix(ci/cd): update gorelease file structure

--- build/upload_artifacts_gh.sh
+++ build/upload_artifacts_gh.sh
@@ -8,5 +8,5 @@ set -e
 cd dist
 find . -regex ".*\.\(msi\|rpm\|deb\|zip\|tar.gz\)" | while read filename; do
   echo "===> Uploading to GH $TAG: ${filename}"
-      gh release upload $TAG $filename
+  gh release upload $TAG $filename
 done
